d94456aeb325b3d6b8375cb15992689728a5d893
Michael Minutelli <mminutel@gmail.com>
2022-11-20 22:53:39 -0500

    Created all of the CRUD elements for Activities.

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,16 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Persistence;
 
 namespace API.Controllers
 {
+
     [ApiController]
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-        
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
     }
 }
---
+++ Application/Activities/List.cs
@@ -0,0 +1,29 @@
+using MediatR;
+using Domain;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> {}
+
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext _context;
+            private readonly ILogger<List> _logger;
+            
+            public Handler(DataContext context, ILogger<List> logger) 
+            {
+                _logger = logger;
+                _context = context;    
+            }
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken) 
+            {
+                return await _context.Activities.ToListAsync();
+            }
+        }
+    }
+}
